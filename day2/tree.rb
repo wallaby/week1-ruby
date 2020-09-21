93ae3d28531fa5b571250365eb948bb4e581d6ce
btoth <balazs@toth.onl>
2020-09-21 21:38:06 +0100

    Add hash based tree builder

--- day2/tree.rb
+++ day2/tree.rb
@@ -6,14 +6,28 @@
 class Tree
   attr_accessor :children, :node_name
 
-  def initialize(name, children=[])
+  def initialize(name, children = [])
     @children = children
     @node_name = name
   end
 
+  def self.tree_from_hash(tree = {})
+    raise('Not a tree!') if tree.size != 1
+
+    forest_from_hash(tree)[0]
+  end
+
+  def self.forest_from_hash(trees = {})
+    forest = []
+    trees.each do |node_name, value|
+      forest.append(Tree.new(node_name, forest_from_hash(value)))
+    end
+    forest
+  end
+
   def visit_all(&block)
-    visit &block
-    children.each {|c| c.visit_all &block}
+    visit(&block)
+    children.each { |c| c.visit_all(&block) }
   end
 
   def visit(&block)
@@ -21,13 +35,18 @@ class Tree
   end
 end
 
-ruby_tree = Tree.new( "Ruby",
-                      [Tree.new("Reia"),
-                       Tree.new("MacRuby")] )
+ruby_tree = Tree.new("Ruby",
+                     [Tree.new("Reia"),
+                      Tree.new("MacRuby")])
 
 puts "Visiting a node"
-ruby_tree.visit {|node| puts node.node_name}
+ruby_tree.visit { |node| puts node.node_name }
 puts
 
 puts "visiting entire tree"
-ruby_tree.visit_all {|node| puts node.node_name}
+ruby_tree.visit_all { |node| puts node.node_name }
+
+ruby_tree2 = Tree.tree_from_hash( {'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } })
+
+puts "second approach"
+ruby_tree2.visit_all { |node| puts node.node_name }
